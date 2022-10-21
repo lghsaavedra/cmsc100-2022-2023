2a71cc73d109e62ea8286e1f8e2200c0d5f196ff
Gail Saavedra <lhsaavedra@up.edu.ph>
2022-10-21 05:53:31 +0000

    feat: add a new server using fastify

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import Fastify from 'fastify';
+
+async function start (){
+    try {
+        const fastify = Fastify({logger:true});
+
+        fastify.get('/api', async(request, reply)=>{
+            return{success: false};
+        });
+        
+        const addr = await fastify.listen({
+            port: '8080'
+        })
+
+        console.log('Listening on $(addr)');
+
+    } catch (error){
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+
+start();
